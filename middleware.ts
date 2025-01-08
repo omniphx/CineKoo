acd830c0b54f4ec08e0581e9d1f0b23ff516ac5d
Matthew Mitchener <mattjmitchener@gmail.com>
2025-01-07 23:48:32 -0800

    Fix middleware

--- middleware.ts
+++ middleware.ts
@@ -1,33 +1,31 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function middleware(request: NextRequest) {
+export default function middleware(request: NextRequest) {
   const basicAuth = request.headers.get("authorization");
 
-  if (request.nextUrl.pathname.startsWith("/admin")) {
-    if (!basicAuth) {
-      return new NextResponse(null, {
-        status: 401,
-        headers: {
-          "WWW-Authenticate": 'Basic realm="Secure Area"',
-        },
-      });
-    }
+  if (!basicAuth) {
+    return new NextResponse(null, {
+      status: 401,
+      headers: {
+        "WWW-Authenticate": 'Basic realm="Secure Area"',
+      },
+    });
+  }
 
-    const authValue = basicAuth.split(" ")[1];
-    const [user, pwd] = atob(authValue).split(":");
+  const authValue = basicAuth.split(" ")[1];
+  const [user, pwd] = atob(authValue).split(":");
 
-    if (
-      user !== process.env.ADMIN_USERNAME ||
-      pwd !== process.env.ADMIN_PASSWORD
-    ) {
-      return new NextResponse(null, {
-        status: 401,
-        headers: {
-          "WWW-Authenticate": 'Basic realm="Secure Area"',
-        },
-      });
-    }
+  if (
+    user !== process.env.ADMIN_USERNAME ||
+    pwd !== process.env.ADMIN_PASSWORD
+  ) {
+    return new NextResponse(null, {
+      status: 401,
+      headers: {
+        "WWW-Authenticate": 'Basic realm="Secure Area"',
+      },
+    });
   }
 
   return NextResponse.next();
